00e79e2106770ea2edb90e6510cf858177729ceb
Ronald Otieno <ronnlats@gmail.com>
2024-05-13 15:46:46 +0300

    a lil padding

--- src/routes/suspension-parts/+page.svelte
+++ src/routes/suspension-parts/+page.svelte
@@ -5,7 +5,7 @@
 	let { products } = data;
 </script>
 
-<div class="bg-black">
+<div class="bg-black py-1">
 	<p class="text-gray-200 text-3xl pl-4 font-semibold py-4">Suspension Parts</p>
 	<div
 		class="w-full  items-center justify-items-center lg:grid-cols-4 mx-auto grid p-4 grid-cols-1 md:grid-cols-2 gap-x-2 lg:gap-7 gap-6 mb-6 justify-center"
