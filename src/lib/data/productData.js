4a26533f6171967e7cd7612faef4100ca4f072af
Ronald Otieno <ronnlats@gmail.com>
2025-06-13 14:16:33 +0300

    new product added(19)

--- src/lib/data/productData.js
+++ src/lib/data/productData.js
@@ -212,6 +212,15 @@ Stabilizer links A.KA.sway bar links, are crucial components of your vehicle’s
 Signs such as rattling sounds especially when driving over bumpy roads or uneven surfaces, uneven tire wear and vibrations in your steering wheel especially during turns are signs that your stabilizer links could be worn out and needs replacement.
 
     `
+	},
+		{
+		id: '19',
+		price: '',
+		name: 'WATER SEPARATOR',
+		category: 'service-parts',
+		imageUrl: 'image18.jpeg',
+		description: `
+An oil water separator plays a crucial role in compressed air systems that use oil as a lubricant. During the process of generating compressed air, condensate (water vapor) is produced. This moisture contains traces of oil and other contaminants. To protect the environment and your equipment, condensate must be treated. After the oil is filtered out, it needs to be separated before it can be safely disposed of, this is where the oil water separator comes in. This equipment separates water and oil within condensate to prevent oil from ending up in the environment.    `
 	}
 	// Add more products as needed
 ];
