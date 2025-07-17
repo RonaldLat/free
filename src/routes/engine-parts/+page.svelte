5772cafe95ef525a159e8019fdfcc3648147a474
Ronald Otieno <ronnlats@gmail.com>
2025-07-17 15:03:42 +0300

    fix

--- src/routes/engine-parts/+page.svelte
+++ src/routes/engine-parts/+page.svelte
@@ -7,7 +7,6 @@
 </script>
 
 <div class="bg-black">
-	<pre>{products}</pre>
 	<p class="text-gray-200 text-3xl pl-4 font-semibold py-4">Engine Parts</p>
 	<div
 		class="w-full items-center justify-items-center lg:grid-cols-4 mx-auto grid p-4 grid-cols-1 md:grid-cols-2 gap-x-2 lg:gap-7 gap-6 mb-6 justify-center"
